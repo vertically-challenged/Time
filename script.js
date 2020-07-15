89ab1c46adb5632c394c0b8b98901332c4184d7f
Anton <anton2chewski@gmail.com>
2020-07-16 01:21:17 +0300

    fix

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function clock () {
     let m = tm.getMinutes();
     let s = tm.getSeconds();
 
+    h = (h < 10) ? `0${h}` : h;
     m = (m < 10) ? `0${m}` : m; 
     s = (s < 10) ? `0${s}` : s;
 
