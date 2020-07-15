e09bb2c2b7909313c7f924964b539ddcff444836
Anton <anton2chewski@gmail.com>
2020-07-15 23:20:19 +0300

    time

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function clock () {
+    let tm = new Date();
+    let h = tm.getHours();
+    let m = tm.getMinutes();
+    let s = tm.getSeconds();
+
+    m = (m < 10) ? `0${m}` : m; 
+    s = (s < 10) ? `0${s}` : s;
+
+    let timeBlock = document.querySelector('.time');
+    let colorBlock = document.querySelector('.color');
+    timeBlock.innerHTML = `${h}:${m}:${s}`;
+    colorBlock.innerHTML = `#${h}${m}${s}`
+
+    document.body.style.backgroundColor = `#${h}${m}${s}`;
+}
+
+(function () {
+    setInterval(() => clock(), 1000);
+})();
